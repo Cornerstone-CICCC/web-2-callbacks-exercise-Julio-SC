c206c1ff6d6369c23277ff37f74e4206a22c8ac9
Julio-SC <jhver2001@gmail.com>
2024-11-19 12:22:04 -0800

    Callback exercise Completed

--- file_reader.js
+++ file_reader.js
@@ -1,5 +1,27 @@
 const fs = require("fs");
 
 fs.readFile("./firstname.txt", "utf-8", (err, data1) => {
-  // your code here
-})
+  if (!err) { 
+      const firstname = data1.trim();
+
+      fs.readFile("./lastname.txt", "utf-8", (err, data2) => {
+          if (!err) {
+              const lastname = data2.trim();
+
+              fs.readFile("./age.txt", "utf-8", (err, data3) => {
+                  if (!err) {
+                      const age = data3.trim();
+
+                      fs.readFile("./hobbies.txt", "utf-8", (err, data4) => {
+                          if (!err) {
+                              const hobbies = JSON.parse(data4.trim()); 
+                              const sentence = `${firstname} ${lastname} is ${age} years old and his hobbies are ${hobbies[0]} and ${hobbies[1]}.`;
+                              console.log(sentence);
+                          }
+                      });
+                  }
+              });
+          }
+      });
+  }
+});
